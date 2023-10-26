21e378e976797f877f2f514e10de281294fba686
Altaf Husain <ahabest20@gmail.com>
2023-10-26 13:21:10 +0530

    testing log

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,15 +5,15 @@ const emailUser = process.env.GMAIL_USER;
 const emailPass = process.env.GMAIL_PASS;
 
 export default async (req, res) => {
-
-  const { name, mobile, email, message } = req.body;
-  console.log('Request: ', req)
-  console.log('Response: ', res)
+  console.log('req: ', req);
+  const { name, email, mobile, message } = req.body;
+  console.log('Request: ', req);
+  console.log('Response: ', res);
 
   // Log some information for debugging
   console.log('Received a request with the following data:');
   console.log('Request: ', req)
-  // console.log('Name:', name);
+  console.log('Name:', name);
   console.log('Email:', email);
   console.log('Mobile:', mobile);
   console.log('Message:', message);
@@ -32,7 +32,7 @@ export default async (req, res) => {
     from: emailUser,
     to: 'papajugnu@gmail.com',
     subject: 'New Contact Form Submission',
-    text: `Email: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
+    text: `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
   };
 
     await new Promise((resolve, reject) => {
