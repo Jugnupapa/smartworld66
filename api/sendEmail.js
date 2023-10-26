ba5d4134186233ced16744f230d85f499c166652
Altaf Husain <ahabest20@gmail.com>
2023-10-26 14:19:27 +0530

    started using formidable

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,4 +1,5 @@
-const nodemailer = require('nodemailer');
+import { IncomingForm } from 'formidable';
+import nodemailer from 'nodemailer';
 
 
 const emailUser = process.env.GMAIL_USER;
@@ -6,56 +7,47 @@ const emailPass = process.env.GMAIL_PASS;
 
 export default async (req, res) => {
   try {
-    console.log('Received request:', req);
+    const form = new IncomingForm();
 
-    const body = req.body;
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ status: 'Error' });
+        return;
+      }
 
-    // Check if 'name' is defined, and if not, provide a default value or handle it as needed.
-    // const senderName = name || 'Anonymous';
+      const { name, email, mobile, message } = fields;
 
-    // Log some information for debugging
-    console.log('Received a request with the following data:');
-    console.log('Request: ', req)
-    console.log('Body: ', body)
-    // console.log('Name:', name);
-    // console.log('Email:', email);
-    // console.log('Mobile:', mobile);
-    // console.log('Message:', message);
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',  // Specify Gmail's SMTP server
-    port: 465,               // Gmail SMTP uses port 465
-    secure: true,            // Use a secure connection
-    auth: {
-        user: emailUser,
-        pass: emailPass
-    },
-  });
-
-  const mailOptions = {
-    from: emailUser,
-    to: 'papajugnu@gmail.com',
-    subject: 'New Contact Form Submission',
-    // text: `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
-    text: `Name: ${req.body}\nEmail: \nMobile: \nMessage: `,
-  };
-
-    await new Promise((resolve, reject) => {
-      // send mail
-      transporter.sendMail(mailOptions, (err, info) => {
-          if (err) {
-              console.error(err);
-              reject(err);
-          } else {
-              console.log(info);
-              resolve(info);
-          }
+      // Create a Nodemailer transporter
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: emailUser,
+          pass: emailPass,
+        },
       });
-  });
-  
-  res.status(200).json({ status: "OK", body: body });
+
+      const mailOptions = {
+        from: emailUser,
+        to: 'papajugnu@gmail.com',
+        subject: 'New Contact Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
+      };
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ status: 'Error' });
+        } else {
+          console.log(info);
+          res.status(200).json({ status: 'OK', body: fields });
+        }
+      });
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ status: "Error" });
+    res.status(500).json({ status: 'Error' });
   }
 };
