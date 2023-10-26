405e1432e52863214792c7a22e7b5eb5b88f3d2c
Altaf Husain <ahabest20@gmail.com>
2023-10-26 12:56:56 +0530

    added try

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,6 +5,15 @@ const emailUser = process.env.GMAIL_USER;
 const emailPass = process.env.GMAIL_PASS;
 
 export default async (req, res) => {
+  console.log('Request: ', req)
+  try {
+    const { name, mobile, email, message } = JSON.parse(req.body);
+    // Rest of your code
+    res.status(200).json({ status: "OK" });
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    res.status(400).json({ error: "Invalid JSON data" });
+  }
 
   const { name, mobile, email, message } = JSON.parse(req.body);
   console.log('Request: ', req)
